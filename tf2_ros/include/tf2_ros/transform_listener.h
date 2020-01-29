0b445a266673ca925310967e4f6ea7e211e4570c
Toni Oliver <toni@shadowrobot.com>
2020-01-29 11:07:21 +0000

    Add TransformListener constructors that accept TransportHints for the tf topic subscriber

--- tf2_ros/include/tf2_ros/transform_listener.h
+++ tf2_ros/include/tf2_ros/transform_listener.h
@@ -52,6 +52,8 @@ public:
   /**@brief Constructor for transform listener */
   TransformListener(tf2::BufferCore& buffer, bool spin_thread = true);
   TransformListener(tf2::BufferCore& buffer, const ros::NodeHandle& nh, bool spin_thread = true);
+  TransformListener(tf2::BufferCore& buffer, ros::TransportHints transport_hints, bool spin_thread = true);
+  TransformListener(tf2::BufferCore& buffer, const ros::NodeHandle& nh, ros::TransportHints transport_hints, bool spin_thread = true);
 
   ~TransformListener();
 
@@ -73,6 +75,7 @@ private:
   ros::Subscriber message_subscriber_tf_static_;
   tf2::BufferCore& buffer_;
   bool using_dedicated_thread_;
+  ros::TransportHints transport_hints_;
   ros::Time last_update_;
  
   void dedicatedListenerThread()
--- tf2_ros/src/transform_listener.cpp
+++ tf2_ros/src/transform_listener.cpp
@@ -56,6 +56,27 @@ TransformListener::TransformListener(tf2::BufferCore& buffer, const ros::NodeHan
     init();
 }
 
+TransformListener::TransformListener(tf2::BufferCore& buffer, ros::TransportHints transport_hints, bool spin_thread):
+  dedicated_listener_thread_(NULL), buffer_(buffer), using_dedicated_thread_(false), transport_hints_(transport_hints)
+{
+  if (spin_thread)
+    initWithThread();
+  else
+    init();
+}
+
+TransformListener::TransformListener(tf2::BufferCore& buffer, const ros::NodeHandle& nh, ros::TransportHints transport_hints, bool spin_thread)
+: dedicated_listener_thread_(NULL)
+, node_(nh)
+, buffer_(buffer)
+, using_dedicated_thread_(false)
+, transport_hints_(transport_hints)
+{
+  if (spin_thread)
+    initWithThread();
+  else
+    init();
+}
 
 TransformListener::~TransformListener()
 {
@@ -69,7 +90,7 @@ TransformListener::~TransformListener()
 
 void TransformListener::init()
 {
-  message_subscriber_tf_ = node_.subscribe<tf2_msgs::TFMessage>("/tf", 100, boost::bind(&TransformListener::subscription_callback, this, _1)); ///\todo magic number
+  message_subscriber_tf_ = node_.subscribe<tf2_msgs::TFMessage>("/tf", 100, boost::bind(&TransformListener::subscription_callback, this, _1), ros::VoidConstPtr(), transport_hints_); ///\todo magic number
   message_subscriber_tf_static_ = node_.subscribe<tf2_msgs::TFMessage>("/tf_static", 100, boost::bind(&TransformListener::static_subscription_callback, this, _1)); ///\todo magic number
 }
 
@@ -77,6 +98,7 @@ void TransformListener::initWithThread()
 {
   using_dedicated_thread_ = true;
   ros::SubscribeOptions ops_tf = ros::SubscribeOptions::create<tf2_msgs::TFMessage>("/tf", 100, boost::bind(&TransformListener::subscription_callback, this, _1), ros::VoidPtr(), &tf_message_callback_queue_); ///\todo magic number
+  ops_tf.transport_hints = transport_hints_;
   message_subscriber_tf_ = node_.subscribe(ops_tf);
   
   ros::SubscribeOptions ops_tf_static = ros::SubscribeOptions::create<tf2_msgs::TFMessage>("/tf_static", 100, boost::bind(&TransformListener::static_subscription_callback, this, _1), ros::VoidPtr(), &tf_message_callback_queue_); ///\todo magic number
